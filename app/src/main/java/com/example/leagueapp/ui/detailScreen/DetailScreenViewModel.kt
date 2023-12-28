fedcf00eecea9f0a6d00f7839293d14c6f89618b
lodeevb <lode.vanbeneden@student.hogent.be>
2023-12-28 20:11:04 +0100

    start detail viewmodel

--- app/src/main/java/com/example/leagueapp/ui/detailScreen/DetailScreenViewModel.kt
+++ app/src/main/java/com/example/leagueapp/ui/detailScreen/DetailScreenViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.leagueapp.ui.detailScreen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.leagueapp.data.ChampionRepository
+import kotlinx.coroutines.flow.StateFlow
+
+class DetailScreenViewModel(private val championRepository: ChampionRepository) : ViewModel() {
+
+    var detailScreenState: DetailScreenState by mutableStateOf(DetailScreenState.Loading)
+        private set
+
+    lateinit var championDetailState: StateFlow<ChampionDetailState>
+}
