df2efcdbbb208e98da6024a7add77f63efdfba1a
lodeevb <lode.vanbeneden@student.hogent.be>
2023-12-28 20:08:57 +0100

    add detailscreenstate

--- app/src/main/java/com/example/leagueapp/ui/detailScreen/DetailScreenState.kt
+++ app/src/main/java/com/example/leagueapp/ui/detailScreen/DetailScreenState.kt
@@ -1,2 +1,14 @@
 package com.example.leagueapp.ui.detailScreen
 
+import com.example.leagueapp.model.ChampionDetail
+
+data class ChampionDetailState(val championDeatil: ChampionDetail)
+
+sealed interface DetailScreenState {
+
+    object Loading : DetailScreenState
+
+    object Error : DetailScreenState
+
+    object Success : DetailScreenState
+}
