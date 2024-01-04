9892443e08aac0579e59afb2bbc5e9e3cd2c1247
lodeevb <lode.vanbeneden@student.hogent.be>
2024-01-04 23:05:12 +0100

    beginning of apitest

---
+++ app/src/test/java/com/example/leagueapp/ChampionApiServiceTest.kt
@@ -0,0 +1,44 @@
+package com.example.leagueapp
+
+import com.example.leagueapp.network.ChampionApi
+import com.example.leagueapp.network.items.ChampionData
+import com.example.leagueapp.network.services.ChampionApiService
+import com.example.leagueapp.network.services.getChampionsAsFlow
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+@ExperimentalCoroutinesApi
+class ChampionApiServiceTest {
+
+    @Test
+    fun getChampionsAsFlow_Success(): runTest {
+
+        val mockChampion1 = ChampionApi("Lode", "Lode",true)
+        val mockChampion2 = ChampionApi("Henk", "Henk")
+
+        val championData = ChampionData(
+            mapOf(
+                "1" to mockChampion1,
+                "2" to mockChampion2
+            )
+        )
+
+        val mockApiService = mock<ChampionApiService> {
+            onBlocking { getChampionData() }.thenReturn(championData)
+        }
+
+        val result = mockApiService.getChampionsAsFlow().toList().first()
+
+        assertEquals(2, result.size)
+        assertEquals("Lode",result[0].name)
+        assertEquals("Henk",result[1].name)
+
+        verify(mockApiService).getChampionData()
+    }
+
+
+}
